b1c4c8bc7c7098e1279c476cd25498ffb52c4c2b
Moamen Zyan <moamenzyan20@gmail.com>
2023-12-07 02:16:59 +0200

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+        }
+    if type(roman_string) is str or roman_string is None:
+        return None
+    total = 0
+    length = len(roman_string)
+    i, j = 0, 1
+    while (i < length):
+        total += dict[roman_string[i]]
+        while (j < length):
+            if dict[roman_string[i]] < dict[roman_string[j]]:
+                total -= dict[roman_string[i]]
+                total += dict[roman_string[j]] - dict[roman_string[i]]
+                break
+            else:
+                total += (dict[roman_string[j]])
+                break
+        i += 2
+        j += 2
+    return (total)
