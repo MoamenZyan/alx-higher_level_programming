f4380178903832d3979df788a3bcc5cce8e77a77
Moamen Zyan <moamenzyan20@gmail.com>
2023-12-19 11:27:57 +0200

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 
+"""This module is for square class."""
+
+
 class Square:
-    """This class is a square class
+    """This class is a square class.
+
     Attributes:
-        __size (int): the size of the square
+        __size (int): the size of the square.
+
     Methods:
-        __init__: To initialize a new instances
+        __init__: To initialize a new instances.
     """
+
     def __init__(self, size=0):
-        """This Method to initialize a new instances
+        """Initialize a new instances.
+
         Args:
-            size (int, optional): the size of the square, Default is 0
+            size (int, optional): the size of the square, Default is 0.
+
         Raises:
-            TypeError: if size is not integer
-            ValueError: if size is lower than 0
+            TypeError: if size is not integer.
+            ValueError: if size is lower than 0.
         """
         if type(size) is int and size >= 0:
             self.__size = size
