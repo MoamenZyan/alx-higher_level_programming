92b9fabbcb2e3070625f107bd75ba9fc6448de53
Moamen Zyan <moamenzyan20@gmail.com>
2023-12-21 15:58:43 +0200

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * *hash_table_get - function to get value of item
+ * @ht: hash table
+ * @key: key of item
+ * Return: value of item
+*/
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -9,39 +15,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	return (ht->array[index]->value);
-}
-
-
-int main(void)
-{
-    hash_table_t *ht;
-    char *value;
-
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "c", "Isfun");
-    hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Bob", "and Kris love asm");
-    hash_table_set(ht, "N", "queens");
-    hash_table_set(ht, "Asterix", "Obelix");
-    hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Street");
-    hash_table_set(ht, "c", "isfun");
-
-    value = hash_table_get(ht, "python");
-    printf("%s:%s\n", "python", value);
-    value = hash_table_get(ht, "Bob");
-    printf("%s:%s\n", "Bob", value);
-    value = hash_table_get(ht, "N");
-    printf("%s:%s\n", "N", value);
-    value = hash_table_get(ht, "Asterix");
-    printf("%s:%s\n", "Asterix", value);
-    value = hash_table_get(ht, "Betty");
-    printf("%s:%s\n", "Betty", value);
-    value = hash_table_get(ht, "98");
-    printf("%s:%s\n", "98", value);
-    value = hash_table_get(ht, "c");
-    printf("%s:%s\n", "c", value);
-    value = hash_table_get(ht, "javascript");
-    printf("%s:%s\n", "javascript", value);
-    return (EXIT_SUCCESS);
 }
