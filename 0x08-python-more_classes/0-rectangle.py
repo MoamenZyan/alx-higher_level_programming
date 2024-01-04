034b6d4f43e13b5ba1e4b9cf56ae4ea287d38d87
Moamen Zyan <moamenzyan20@gmail.com>
2024-01-04 20:28:38 +0200

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This is my class."""
+
+
+class Rectangle:
+    """Rectangle class."""
+
+    pass
+
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
