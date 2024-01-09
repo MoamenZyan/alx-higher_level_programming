4a07cc6d5a068fb1353d3c49bd3d6a55fae84a96
Moamen Zyan <moamenzyan20@gmail.com>
2024-01-09 02:38:29 +0200

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""My function."""
+
+
+class MyList(list):
+    """To sort list."""
+
+    def print_sorted(self):
+        """Print the sorted list."""
+        print(sorted(self))
