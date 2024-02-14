ffd75063f90de8f15d09429154f03e93d1f31a3e
Moamen Zyan <moamenzyan20@gmail.com>
2024-02-15 01:38:51 +0200

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+-- Use the new database
+USE hbtn_0d_usa;
+-- Create the table
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL);
