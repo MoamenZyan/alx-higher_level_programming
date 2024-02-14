c4f4d92434728e72416f0816b55f04928899b046
Moamen Zyan <moamenzyan20@gmail.com>
2024-02-15 01:27:00 +0200

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Create Database 'hbtn_0d_2'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create user 'user_0d_2'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
