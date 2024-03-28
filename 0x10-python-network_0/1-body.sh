7e90b0b32e4e431c6cbbc87e3a68015454d1dd0d
MoamenZyan <moamenzyan20@gmail.com>
2024-03-28 03:35:24 +0200

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL
 
-status_code=$(curl -s -o /dev/null -w "%{http_code}\n" "$1")
-
-if [ "$status_code" -eq 200 ];
-then
-    curl -s "$1"
-fi
+curl -s -f "$1"
